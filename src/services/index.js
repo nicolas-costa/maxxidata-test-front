b3835e95d91f8b550a81dad32eeccd3f28f4cf3b
Nícolas Costa <nicolas.nico@hotmail.com>
2021-03-17 09:01:20 -0300

    Implementação inicial da camada de services para comunicação com a api.

---
+++ src/services/index.js
@@ -0,0 +1,10 @@
+import env from "react-dotenv";
+import axios from 'axios';
+
+const API_URL = `${env.API_ADDRESS}:${env.API_PORT}/api/`;
+
+const api = axios.create({
+    baseURL: API_URL
+});
+
+export default api;
---
+++ src/services/professionalType.js
@@ -0,0 +1,17 @@
+import api from './';
+
+const ENDPOINTS = {
+    professionalTypes: 'tipo-de-profissional'
+};
+
+export const getAll = async () => {
+    try {
+        const res = await api.get(ENDPOINTS.professionalTypes);
+
+        if(res.status === 200) {
+            return res.data
+        } else return []
+    } catch (e) {
+        return null;
+    }
+};

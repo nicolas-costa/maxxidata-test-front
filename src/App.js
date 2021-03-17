a9def9ab203edf5685589ff4fc9ee1449e9ef50e
Nícolas Costa <nicolas.nico@hotmail.com>
2021-03-16 23:24:23 -0300

    Adicionado esqueleto da tela inicial e sua rota.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SideMenu from "./components/molecules/SideMenu";
+import Main from "./components/atoms/Main";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+  root: { display: "flex" },
+});
 
 function App() {
+  const classes = styles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <BrowserRouter>
+        <CssBaseline />
+        <SideMenu />
+        <Main>
+          <Routes />
+        </Main>
+      </BrowserRouter>
     </div>
   );
 }

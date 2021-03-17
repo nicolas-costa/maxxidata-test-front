fe130576213cc458bd6bb29972edcebb59d40275
Nícolas Costa <nicolas.nico@hotmail.com>
2021-03-17 09:04:30 -0300

    Implementada tela de listagem de tipo de profissional.

--- src/pages/professionalType/index.js
+++ src/pages/professionalType/index.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Typography } from "@material-ui/core";
 import Table from "../../components/molecules/Table";
+import { getAll } from '../../services/professionalType';
 
 const ProfessionalType = (props) => {
   const columns = [
-    { field: "id", headerName: "Id", width: 70 },
-    { field: "descricao", headerName: "Descrição", width: 350 },
-    { field: "situacao", headerName: "Situação", width: 150 },
+    { field: "id", headerName: "Id", sortable: false, width: 70 },
+    { field: "descricao", headerName: "Descrição", sortable: false, width: 350 },
+    { field: "situacao", headerName: "Situação", sortable: false, width: 150,
+        valueGetter: (params) =>
+            params.situacao ? 'Ativo' : 'Inativo'
+    } ,
   ];
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(async () => {
+      const res = await getAll();
+
+      if(res) {
+          setRows(res);
+      }
+  }, []);
 
   return (
     <div>
       <Typography variant="h4">Tipos de profissional</Typography>
-      <Table columns={columns} />
+      <Table columns={columns} rows={rows}/>
     </div>
   );
 };

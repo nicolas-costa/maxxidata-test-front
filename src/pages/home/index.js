33dc9017cd6a4277c39d2cbf6065259a21c25a3f
Nícolas Costa <nicolas.nico@hotmail.com>
2021-03-17 00:50:17 -0300

    Ajustes no conteúdo da tela Início.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import { Typography } from "@material-ui/core";
 
 const Home = (props) => {
-
-
     return (
-      <h1>Home</h1>
+        <div>
+            <Typography variant='h4'>Início</Typography>
+            <Typography paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus,
+                nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem
+                felis nec erat
+            </Typography>
+        </div>
+
     );
 };
 

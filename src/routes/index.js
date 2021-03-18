135e71673fc27aa93d0d9d56f15b6ae6906f1205
Nícolas Costa <nicolas.nico@hotmail.com>
2021-03-18 00:29:57 -0300

    Adicionada rota para a tela de criação de novo profissional.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import Home from "../pages/home";
-import Professional from "../pages/professional";
-import ProfessionalType, { New } from "../pages/professionalType";
+import Professional, { New as NewProfessional } from "../pages/professional";
+import ProfessionalType, { New as NewProfessionalType } from "../pages/professionalType";
 
 const Routes = () => {
   return (
@@ -9,7 +9,8 @@ const Routes = () => {
       <Route exact path="/" component={Home} />
       <Route path="/profissionais" component={Professional} />
       <Route path="/tipos-de-profissional" component={ProfessionalType} />
-      <Route path="/novo-tipo-de-profissional" component={New} />
+        <Route path="/novo-profissional" component={NewProfessional} />
+      <Route path="/novo-tipo-de-profissional" component={NewProfessionalType} />
     </Switch>
   );
 };

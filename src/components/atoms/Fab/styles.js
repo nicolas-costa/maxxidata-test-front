ecdb9b337562bdc3a48d2ce9dbf95ea3c660a9bd
Nícolas Costa <nicolas.nico@hotmail.com>
2021-03-17 21:03:48 -0300

    Criado componente Fab.

---
+++ src/components/atoms/Fab/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles((theme) => ({
+  fab: {
+    position: "fixed",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+export default styles;
